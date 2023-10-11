d6371aae6e962772f299a4208b7627faa7f661cf
Gloria Caminades <gloria.caminades@vol.atgit config --global user.email>
2023-10-11 10:57:17 +0200

    First

---
+++ src/at/gloriac/basis/Class.java
@@ -0,0 +1,14 @@
+package at.gloriac.basis;
+
+public class Class {
+
+    public static void main(String[] args) {
+        System.out.println("Hallo");
+
+   //short
+   //int - ganze zahl
+
+    }
+    //strg blank
+}
+
