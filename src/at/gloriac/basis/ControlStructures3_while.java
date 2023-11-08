8506575fe43ab1d00b8db4c2d13cdbfb35f96c94
glori <gloria.caminades@vol.atgit config --global user.email>
2023-11-08 11:21:59 +0100

    ControlStructures3_loop 1 und 2 erstellt

---
+++ src/at/gloriac/basis/ControlStructures3_while.java
@@ -0,0 +1,30 @@
+package at.gloriac.basis;
+
+import java.util.Random;
+
+public class ControlStructures3_while {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        boolean isFinished = false;
+
+        while (!isFinished) {
+
+            int low = 10;
+            int high = 30;
+            int result = random.nextInt(high-low) + low;
+
+            System.out.println("Nr:" + result);
+            if (result == 15) {
+                isFinished = true;
+            }
+            else if (result == 25) {
+                isFinished = true;
+            }
+        } System.out.println("Finished");
+
+    }
+
+}
+
