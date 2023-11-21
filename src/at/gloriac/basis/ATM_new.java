b50d1fa43eca37de35895a4a1211ace51cc8439d
glori <gloria.caminades@vol.atgit config --global user.email>
2023-11-21 19:12:38 +0100

    ControlStructures3_while erstellt

--- src/at/gloriac/basis/ATM_new.java
+++ src/at/gloriac/basis/ATM_new.java
@@ -5,57 +5,53 @@ import java.util.Scanner;
 public class ATM_new {
     public static void main(String[] args) {
         {
-            //declare and initialize balance, withdraw, and deposit
             int balance = 100000, withdraw, deposit;
 
-            //create scanner class object to get choice of user
             Scanner sc = new Scanner(System.in);
 
             while (true) {
 
                 System.out.println("Selektiren Sie die gewünschte Funktion");
-                System.out.println("1. Einzahlen");
-                System.out.println("2. Abheben");
-                System.out.println("Choose 4 for EXIT");
-                System.out.print("Choose the operation you want to perform:");
+                System.out.println("1. Abheben");
+                System.out.println("2. Einzahlen");
+                System.out.println("3. Kontostand anzeigen");
+                System.out.print("4. Schließen");
 
-                //get choice from user
+
                 int choice = sc.nextInt();
                 switch (choice) {
                     case 1:
-                        System.out.print("Enter money to be withdrawn:");
+                        System.out.print("Geld eingeben, dass ausgezalt werden soll:");
 
-                        //get the withdrawl money from user
                         withdraw = sc.nextInt();
 
-                        //check whether the balance is greater than or equal to the withdrawal amount
+
                         if (balance >= withdraw) {
-                            //remove the withdrawl amount from the total balance
+
                             balance = balance - withdraw;
-                            System.out.println("Please collect your money");
+                            System.out.println("Ihr Kontostand beträgt:"+balance);
                         } else {
-                            //show custom error message
-                            System.out.println("Insufficient Balance");
+
+                            System.out.println("Falsche Eingabe");
                         }
                         System.out.println("");
                         break;
 
                     case 2:
 
-                        System.out.print("Enter money to be deposited:");
+                        System.out.print("Geld eingeben, dass ausgezalt werden soll:");
 
-                        //get deposite amount from te user
                         deposit = sc.nextInt();
 
-                        //add the deposit amount to the total balanace
+
                         balance = balance + deposit;
-                        System.out.println("Your Money has been successfully depsited");
+                        System.out.println("Ihr Geld wurde erfolgreich eingezahlt!");
                         System.out.println("");
                         break;
 
                     case 3:
                         //displaying the total balance of the user
-                        System.out.println("Balance : " + balance);
+                        System.out.println("Kontostand : " + balance);
                         System.out.println("");
                         break;
 
